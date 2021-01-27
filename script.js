aa0a720260734f3f753ba65bfbaf9d5875fa2bf4
TiarraWashington <kenniyah11@gmail.com>
2021-01-27 15:25:39 -0600

    updates grid row and column

--- script.js
+++ script.js
@@ -4,24 +4,34 @@ let playerOne = document.querySelector("#playerOne");
 let playerTwo = document.querySelector("#playerTwo");
 let leadPlayer = document.querySelector("#leadPlayer");
 let clickPlayer = document.querySelector("#clickPlayer");
+// seleceted buttons using querySelector
 let playeroneBtn = document.querySelector("#playeroneBtn");
 let playertwoBtn = document.querySelector("#playertwoBtn");
 
 // console log element/placeholders
-// console.log(playerOne); 
-// console.log(playerTwo); 
-// console.log(leadPlayer); 
-// console.log(clickPlayer); 
-// console.log(playeroneBtn);
-// console.log(playertwoBtn);
+console.log(playerOne);
+console.log(playerTwo);
+console.log(leadPlayer);
+console.log(clickPlayer);
+// console log for buttons
+console.log(playeroneBtn);
+console.log(playertwoBtn);
+
+// add score in appropriate element
+if () {
+
+} else {
+
+}
 
 function addone() {
-
+    if ()
 }
 
 
 // function tie() {
 //     if ()
 // }
+
 
 
