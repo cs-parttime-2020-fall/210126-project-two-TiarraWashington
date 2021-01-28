452613b53c091b05ed476ced125c3c08cb39cfd1
TiarraWashington <kenniyah11@gmail.com>
2021-01-28 00:18:15 -0600

    working on reset for game

--- script.js
+++ script.js
@@ -24,33 +24,45 @@ let playertwo = document.querySelector("#playertwo");
 
 let score1 = 0;
 let score2 = 0;
+let winningScore = 10;
+
 // create function to add 1 when player is clicked 
 function addOne() {
     score1++;
     // console.log(score1);
     playerone.innerHTML = score1;
     updateleadPlayer();
+    winningScore = 10;
 }
 
 function addTwo() {
     score2++;
     playertwo.innerHTML = score2;
     updateleadPlayer();
+    winningScore = 10;
 }
 
 function updateleadPlayer() {
     console.log(score1); console.log(score2);
     if (score1 == score2) {
-        leadPlayer.innerHTML = "tied"
-        console.log("tied");
+        leadPlayer.innerHTML = "TIED"
+        console.log("TIED");
     } else if (score1 > score2) {
-        leadPlayer.innerHTML = "player one in the lead"
-        console.log("player one in the lead");
+        leadPlayer.innerHTML = "Player one in the lead"
+        console.log("Player one in the lead");
     } else if (score1 < score2) {
-        leadPlayer.innerHTML = "player two is in the lead"
-        console.log("player two is in the lead");
+        leadPlayer.innerHTML = "Player two is in the lead"
+        console.log("Player two is in the lead");
     }
+    function winningScore () {
+}  if (score1 == "winningScore") {
+    leadPlayer.innerHTML = "winningScore"
+    alert("Player one is in the lead");
+} else if (score2 == "10") {
+    leadPlayer.innerHTML = "winningScore"
+    alert("Player two is in the lead");
 }
+
 
 playeroneBtn.addEventListener("click", addOne);
 playertwoBtn.addEventListener("click", addTwo);
