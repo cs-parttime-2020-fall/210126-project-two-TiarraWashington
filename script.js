850cf9ff8b2703caa0e01e4c159efaf1de8b5bc5
TiarraWashington <kenniyah11@gmail.com>
2021-01-27 18:22:00 -0600

    add function to increase score

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 console.log(`21 01 26 HTML CSS JS Project Two`);
+
 // select element/placeholders using querySelector
 let playerOne = document.querySelector("#playerOne");
 let playerTwo = document.querySelector("#playerTwo");
 let leadPlayer = document.querySelector("#leadPlayer");
-let clickPlayer = document.querySelector("#clickPlayer");
 // seleceted buttons using querySelector
 let playeroneBtn = document.querySelector("#playeroneBtn");
 let playertwoBtn = document.querySelector("#playertwoBtn");
@@ -12,26 +12,25 @@ let playertwoBtn = document.querySelector("#playertwoBtn");
 console.log(playerOne);
 console.log(playerTwo);
 console.log(leadPlayer);
-console.log(clickPlayer);
 // console log for buttons
 console.log(playeroneBtn);
 console.log(playertwoBtn);
 
-// add score in appropriate element
-if () {
 
-} else {
+// create function to add 1 when player is clicked 
+ function addone()  {
+     let foo = document.getElementById(`playerone`).innerHTML
+     foo++;
+     document.getElementById(`playerone`).innerHTML = foo;
+} 
 
-}
-
-function addone() {
-    if ()
-}
-
-
-// function tie() {
+// function updateleadPlayer() {
 //     if ()
 // }
 
+function resetGame() {
 
+}
+
+playeroneBtn.addEventListener("click", addone);
 
