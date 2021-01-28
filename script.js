81284b9e7a9a52bf1fee0ac5219b21a8ecdaa7d1
TiarraWashington <kenniyah11@gmail.com>
2021-01-27 19:11:10 -0600

    updated score appropriate to button clicked

--- script.js
+++ script.js
@@ -4,33 +4,46 @@ console.log(`21 01 26 HTML CSS JS Project Two`);
 let playerOne = document.querySelector("#playerOne");
 let playerTwo = document.querySelector("#playerTwo");
 let leadPlayer = document.querySelector("#leadPlayer");
+// console log element/placeholders
+// console.log(playerOne);
+// console.log(playerTwo);
+// console.log(leadPlayer);
+
 // seleceted buttons using querySelector
 let playeroneBtn = document.querySelector("#playeroneBtn");
 let playertwoBtn = document.querySelector("#playertwoBtn");
+// // console log for buttons
+// console.log(playeroneBtn);
+// console.log(playertwoBtn);
 
-// console log element/placeholders
-console.log(playerOne);
-console.log(playerTwo);
-console.log(leadPlayer);
-// console log for buttons
-console.log(playeroneBtn);
-console.log(playertwoBtn);
+// select player score using querySelector
+let playerone = document.querySelector("#playerone");
+let playertwo = document.querySelector("#playertwo");
+// console.log(playerone);
+// console.log(playertwo);
 
+let score1 = 0;
+let score2 = 0;
 
 // create function to add 1 when player is clicked 
- function addone()  {
-     let foo = document.getElementById(`playerone`).innerHTML
-     foo++;
-     document.getElementById(`playerone`).innerHTML = foo;
+function addOne() {
+    score1++;
+    // console.log(score1);
+    playerone.innerHTML = score1;
 } 
+
+function  addTwo() {
+    score2++;
+    playertwo.innerHTML = score2;
+}
 
 // function updateleadPlayer() {
 //     if ()
 // }
 
-function resetGame() {
+// function resetGame() {
 
-}
+// }
 
-playeroneBtn.addEventListener("click", addone);
-
+playeroneBtn.addEventListener("click", addOne);
+playertwoBtn.addEventListener("click", addTwo);
